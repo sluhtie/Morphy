5e6aa5434affc465aebc0ababa34f0e04cb8342b
Connor W <sluhtie@proton.me>
2024-03-14 02:20:12 +0100

    Make cogs loading static

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from logger import logger
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot(intents=discord.Intents.all())
 
-for cog in (cogs := [cog for cog in pathlib.Path('cogs').iterdir() if cog.name != '__pycache__']):
-    cogstring = str(cog).replace('/', '.')
+# for cog in (cogs := [cog for cog in pathlib.Path('cogs').iterdir() if cog.name != '__pycache__']):
+for cog in ["cogs.userInfo", "cogs.purgeMessages"]:
     try:
-        bot.load_extension(cogstring.replace('.py', ''))
-        logger.info(f'Loaded cog: {cogstring}')
+        bot.load_extension(cog)
+        logger.info(f'Loaded cog: {cog}')
     except FileNotFoundError as ex:
         logger.error(f"Error. The file does not exist: {ex}")
     except PermissionError as ex:

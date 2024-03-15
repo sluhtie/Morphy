1ba973ccd6e362eb7b7750132d088ae93d411834
Connor W <sluhtie@proton.me>
2024-03-15 15:51:44 +0100

    Make button function static

--- views/lookupUser.py
+++ views/lookupUser.py
@@ -2,8 +2,10 @@ import discord
 from discord.ui import Button
 
 
-class lookupUserBTN(discord.ui.View):
+async def button(label='User Lookup', style=discord.ButtonStyle.link, url=None):
+    return discord.ui.Button(label=label, style=style, url=url)
 
-    async def button(self, label='User Lookup', style=discord.ButtonStyle.link, url=None):
-        return discord.ui.Button(label=label, style=style, url=url)
+
+class lookupUserBTN(discord.ui.View):
+    pass
 
